25255634e21832aecad886d6c15563c3d9a35b16
NathanSP1 <nathansprather@gmail.com>
2022-04-20 21:34:16 -0700

    Calculating Examples

---
+++ LearningActivites/Calculations.py
@@ -0,0 +1,33 @@
+#Addition 
+#prints out to be 644
+print(433+211)
+#prints out to be 186
+print(122+43+21)
+#prints out to be 10
+print(1+2+3+4)
+
+
+
+#Subtraction 
+#prints out to be 22
+print(54-32)
+#prints out to be 400
+print(521-23-98)
+
+
+
+#Division 
+#prints out to be 8
+print(40/5)
+#prints out to be 6.25
+print(25/4)
+#prints out to be 7.1666
+print(43/6)
+
+
+
+#Multiplecation 
+#prints out to be 92
+print(4*23)
+#prints out to be 756
+print(54*2*7)

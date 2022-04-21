639bf58ca3d94308c95b170ee3a8d83992adc4d1
NathanSP1 <nathansprather@gmail.com>
2022-04-20 20:49:28 -0700

    Activity

--- Helloworld.py
+++ Helloworld.py
@@ -1,1 +1,4 @@
 print("Hello World")
+
+my_name = "Nathan"
+print("Hello World, My name is " + my_name + "!")
---
+++ LearningActivites/VariableEx.py
@@ -0,0 +1,7 @@
+message_string = "Hello World"
+#Prints "hellow world"
+print(message_string)
+
+#Greeting
+message_greeting = "Hello There"
+print(message_greeting)

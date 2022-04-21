061122b762820eb1e35117552a024c5af4318350
NathanSP1 <nathansprather@gmail.com>
2022-04-20 21:59:44 -0700

    ExponentEx

---
+++ LearningActivites/Exponents.py
@@ -0,0 +1,17 @@
+# 2 to the power of 10
+print(2 ** 10)
+
+# 6 to the power of 9
+print(6 ** 9)
+
+# 6 to the power of 6
+print(6 ** 6)
+
+# 7 to the power of 7
+print(7 ** 7)
+
+#8 to the power of 8
+print(8 ** 8)
+
+#calculating 6x6 tiles by the quantity of 6 orders from 6 people
+print(6 ** 4)

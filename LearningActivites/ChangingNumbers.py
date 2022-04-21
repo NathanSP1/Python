02e18371b597b6170edcde4dead26631b6a1d9e1
NathanSP1 <nathansprather@gmail.com>
2022-04-20 21:45:02 -0700

    Changing Numbers Activity

---
+++ LearningActivites/ChangingNumbers.py
@@ -0,0 +1,12 @@
+#Define the length and the width of the quilt tiles
+quilt_width = 8
+quilt_length = 12
+
+#Calculate the total number ff squares needed to be created
+print(quilt_width * quilt_length)
+
+#Updated material count to 8 tiles long
+quilt_length = 8
+
+#Calculate the total updated number of squares needed for the quilt
+print(quilt_length * quilt_width)

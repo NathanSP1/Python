c133a88709c4a7eb0000c366b9393ee46d595626
NathanSP1 <nathansprather@gmail.com>
2022-04-20 21:16:30 -0700

    IntegerEx

---
+++ LearningActivites/Integers.py
@@ -0,0 +1,18 @@
+#Define the movie name
+movie = 'CityOfTroy'
+
+#Define the runtime of the movie
+runtime = 120
+
+#Defines the release year of the movie
+release_year = 2018
+
+#Define the rating the movie recieved 
+rating_out_of_10 = 9.0
+
+#Print out the operations 
+print("The Movie is called: " + movie)
+
+print(runtime)
+print(release_year)
+print(rating_out_of_10)

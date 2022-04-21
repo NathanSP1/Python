6a9a00be941ae1bd9b0fc04ca5a19a5a6627d037
NathanSP1 <nathansprather@gmail.com>
2022-04-20 22:11:43 -0700

    Modulo Operations Activity

---
+++ LearningActivites/ModuloOperators.py
@@ -0,0 +1,16 @@
+#prints out to be 5 * 12 with the remainder of 3
+print(63 % 5)
+#prints out to be 6 * 7 with a remainder of 5
+print(47 % 6)
+#prints out to be 7 * 4 with a remainder of 0
+print(28 % 7)
+#prints out to be 5 * 2 with a remainder of 2
+print(12 % 5)
+
+#Try dividing a group of 27 people into 4 teams while saving the value as my_team
+my_team = 27 % 4
+print(my_team)
+
+#what number team are the two people next to you (26 and 28) on? What are the numbers for all 4 teams?
+print(28 % 4)
+print(26 % 4)

4417e1dd07a2f695ae34d78927de36ea546e9cc9
NathanSP1 <nathansprather@gmail.com>
2022-04-20 22:40:22 -0700

    Performing mathmatical functions, messages that store variables, and other reviewed material in this project

---
+++ Projects/Project1.py
@@ -0,0 +1,16 @@
+#Define a variable of your age called 'my_age'
+my_age = 22
+
+#Define a variable that outputs half of your age called 'half_my_age'
+half_my_age = 22 / 2
+
+#Define a greeting statement called 'greeting'
+greeting = "Hello there, "
+
+#Define the name using the variable 'name'
+name = "Nathan"
+
+#Define a greeting statement that include the name called 'greeting_with_name'
+greeting_with_name = "It's nice to meet you Morty"
+
+print(greeting, "My name is " , name, "I Am" , my_age, "I have a cousin that is half of my age at ", half_my_age ,greeting_with_name)

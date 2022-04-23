f8552eff25115bd511208ded2db9a939fb0f2a60
NathanSP1 <nathansprather@gmail.com>
2022-04-22 18:53:39 -0700

    'and' operation practive activity

---
+++ LearningActivites/andOperatorPractice.py
@@ -0,0 +1,12 @@
+#Statement One
+(6*2-3 >= 13) and (20 / 2 == 10) #Statement is false
+
+#Statement Two
+(2*2-1 < 5) and (4-1-2 >= 1) #Staement is True
+
+#Using 'and' in an IF statement
+money = 37.24
+item = 27.32
+
+if money <= 37.24 and item <= 37.24:
+      print("You can buy the item")

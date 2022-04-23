482d2289d70d5865d0c8956f76ad8c5604ee14fc
NathanSP1 <nathansprather@gmail.com>
2022-04-22 18:15:30 -0700

    If and Else Statement practice including user input

--- LearningActivites/IFStatementPractice.py
+++ LearningActivites/IFStatementPractice.py
@@ -1,8 +1,13 @@
 #Enter a username 
 user_name = (input("what is your username?: "))
 
+#If the User is Dave then print message indicating him to get off of the computer 
 if user_name == "Dave":
     print("Get off of my computer")
 
+#If enterd name is angela_87 then it is possibly dave using someone else's account to access the computer so print a message indicating that you know it's Dave
+if user_name == "angela_87":
+    print("I know it's you Dave!!!")
+    
 else:
     print("Hello User: " + user_name)

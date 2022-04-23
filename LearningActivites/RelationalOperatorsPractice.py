0cd75d753f4fc3ee5c094220f876f82216a5cdda
NathanSP1 <nathansprather@gmail.com>
2022-04-22 18:28:11 -0700

    RelationOperations practice activity

---
+++ LearningActivites/RelationalOperatorsPractice.py
@@ -0,0 +1,13 @@
+Toy1 = 25
+Toy2 = 25
+
+# Write the first if statement here:
+if Toy1 == Toy2:
+  print("The prices of these toys are the same")
+
+
+college_credits = 120
+
+# Write the second if statement here:
+if college_credits >= 120:
+  print("You have enough credits to graduate you program!")

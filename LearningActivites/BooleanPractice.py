a67e54be78c661abf633ff7defbb7556d6995232
NathanSP1 <nathansprather@gmail.com>
2022-04-22 17:23:46 -0700

    Creates Reciepts

---
+++ LearningActivites/BooleanPractice.py
@@ -0,0 +1,11 @@
+#Sets a variable named my_bool to True
+my_bool = True
+
+#Checks the type of my_bool 
+print(type(my_bool))
+
+#Sets a variable named my_bool to fasle
+my_bool_two = False
+
+#Checks the type of my_bool_two
+print(type(my_bool_two))

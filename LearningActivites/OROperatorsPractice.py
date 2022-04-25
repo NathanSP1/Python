e10382afb77a8b64ce66ebef5f7acb04754d975d
NathanSP1 <nathansprather@gmail.com>
2022-04-25 12:23:54 -0700

    OR operator practice example

---
+++ LearningActivites/OROperatorsPractice.py
@@ -0,0 +1,13 @@
+statement_one = (2 - 1 > 3) or (-5 * 2 == -10)
+
+
+statement_two = (9 + 5 <= 15) or (7 != 4 + 3)
+
+
+credits = 118
+gpa = 2.0
+
+#Write an if statement that checks if a student either has 120 or more credits or a GPA 2.0 or higher
+
+if credits >= 120 or gpa >= 2.0:
+  print("You have met at least one of the requirements.")

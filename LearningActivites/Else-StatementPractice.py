6f0ea4efccf2ff1560398592421c88d2018a315c
NathanSP1 <nathansprather@gmail.com>
2022-04-25 12:44:10 -0700

    Else statement practice example

---
+++ LearningActivites/Else-StatementPractice.py
@@ -0,0 +1,9 @@
+# Else statements allow us to elegantly describe what we want our code to do when certain conditions are not met; always appear in conjunction with if statements
+
+credits = 120
+gpa = 1.9
+
+if (credits >= 120) and (gpa >= 2.0):
+  print("You meet the requirements to graduate!")
+else: 
+  print("You do not meet the requirements to graduate.")

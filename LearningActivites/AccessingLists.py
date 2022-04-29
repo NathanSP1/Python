318ae4c639fb1d3b1068199bcc3653b140cfd9b0
NathanSP1 <nathansprather@gmail.com>
2022-04-28 21:56:59 -0700

    Accessing List practice

---
+++ LearningActivites/AccessingLists.py
@@ -0,0 +1,10 @@
+employees = ["Michael", "Dwight", "Jim", "Pam", "Ryan", "Andy", "Robert"]
+
+#Refer to the 4th employee on the list... index starts at 0
+employee_four = (employees[3])
+
+#prints employee 7 from the list of names 
+print(employees[6])
+
+#prints employee_four
+print(employee_four)

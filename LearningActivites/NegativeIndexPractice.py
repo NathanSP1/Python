df70822ae853ea43ebb7f9bd30f89b11cbfe533b
NathanSP1 <nathansprather@gmail.com>
2022-04-28 22:11:36 -0700

    Access list elements using negative Index practice code

---
+++ LearningActivites/NegativeIndexPractice.py
@@ -0,0 +1,12 @@
+#List of shopping Items
+shopping_list = ["eggs", "butter", "milk", "cucumbers", "juice", "cereal"]
+
+#Have a last_element variable be assigned to the last element on the shopping list by using -1 (Note: on a list of 6, the last item starts at -1 and the first item would be -6)
+last_element = (shopping_list[-1])
+
+#Selecting the 5th element out of the shopping list using the variable index_element (Note: Remember that with index numbering, the starting number is 0 meaning that eggs = 0)
+index5_element = (shopping_list[-1])
+
+#Print both variables 
+print(last_element)
+print(index5_element)
